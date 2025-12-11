d1a9055b1cd7681f694a9a3014ab84cc3ac7f512
Saurabh Nagar <saurabhk2890@gmail.com>
2025-12-11 22:04:11 +0530

    add env file

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,9 @@
 import express, {} from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.use("/", (req, res) => {
-    res.send("Hello...");
+    res.send("Done deploy on ec2 server..." + process.env.SECRET);
 });
 app.listen(3000, () => {
     console.log("Server is running...");
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express, { type Request, type Response } from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 app.use("/", (req: Request, res: Response) => {
-  res.send("Done deploy on ec2 server...");
+  res.send("Done deploy on ec2 server..."+process.env.SECRET);
 });
 
 app.listen(3000, () => {
